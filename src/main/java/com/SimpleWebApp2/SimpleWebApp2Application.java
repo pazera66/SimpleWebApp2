ef152993581536fd859a52db534b116dc3c291a6
pazera66 <k.p.krol@stud.elka.pw.edu.pl>
2016-07-18 15:42:10 +0200

    initial commit

---
+++ src/main/java/com/SimpleWebApp2/SimpleWebApp2Application.java
@@ -0,0 +1,12 @@
+package com.SimpleWebApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApp2Application.class, args);
+	}
+}
